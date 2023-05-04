d365e5d1015ff9a8897a3edc1e8983843e24045b
davjh <dillonsdad91@gmail.com>
2023-05-03 22:51:41 -0400

    adding framer

--- src/App.js
+++ src/App.js
@@ -26,10 +26,31 @@ import './App.css';
 
 // export default App;
 
+
+const [selectedId, setSelectedId] = useState(null)
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
   )
+
+  {items.map(item => (
+    <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
+      <motion.h5>{item.subtitle}</motion.h5>
+      <motion.h2>{item.title}</motion.h2>
+    </motion.div>
+  ))}
+  
+  <AnimatePresence>
+    {selectedId && (
+      <motion.div layoutId={selectedId}>
+        <motion.h5>{item.subtitle}</motion.h5>
+        <motion.h2>{item.title}</motion.h2>
+        <motion.button onClick={() => setSelectedId(null)} />
+      </motion.div>
+    )}
+  </AnimatePresence>
+  
 }
