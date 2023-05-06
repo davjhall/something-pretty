c992f154c1ff082ddf171569df9aa272ed81ff73
davjh <dillonsdad91@gmail.com>
2023-05-06 14:18:27 -0400

    adding routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 
 export default function App() {
   return (
+    <>
     
     <div class="navbar bg-primary text-primary-content">
+      <Headder/>
   <a class="btn btn-ghost normal-case text-xl">PRESENTING...</a>
 </div>
+<Routes>
+<Route path="/" element={<Designs />}></Route>
 
-
-
+</Routes>
+<Footer/>
+    <div>A Project made with effort and dedication</div>
+</>
     
   )
   }
