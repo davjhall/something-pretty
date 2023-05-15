62254d2de0a13f3380f962a2343b47036e515fa2
davjh <dillonsdad91@gmail.com>
2023-05-14 23:42:27 -0400

    Changed footer color

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -12,7 +12,7 @@ import {
 export default function Footer() {
   return (
 
-    <MDBFooter className='bg-light text-center text-white'>
+    <MDBFooter className='bg-dark text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
           
