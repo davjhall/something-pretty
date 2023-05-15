b60c3fd2d4145830f4a24e788bba3f1593023923
davjh <dillonsdad91@gmail.com>
2023-05-15 03:47:51 -0400

    added clickable resume pdf file

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,14 +1,17 @@
 import React from "react";
-import logo from "./ResumeNew.png"
+import logo from "./ResumeNew.png";
+import Pdf from "../Components/MyResume.pdf";
 
 
 
 export  default function Resume() {
+    let url = "MyResume.pdf"
     return (
         <div className="dhall">
-        <img src={logo} alt="resume" className="bg-image hover-overlay hover-zoom hover-shadowv dhall " height={350}
-    width={700}
-    style={{ alignSelf: 'center' }} />
+        <a href={Pdf}  target="_blank">
+        <img src={logo} alt="resume" className="bg-image hover-overlay hover-zoom hover-shadow" 
+        style={{ alignSelf: 'center' }} />
+        </a>
     </div>
     );
 }
